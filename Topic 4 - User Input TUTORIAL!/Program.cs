dc19d2cc8cb37be27b31a230c0f6094c069844e2
Sophie Jean <363449299@GoTVDSB.ca>
2025-02-26 14:22:38 -0500

    finished tutorial

--- Topic 4 - User Input TUTORIAL!/Program.cs
+++ Topic 4 - User Input TUTORIAL!/Program.cs
@@ -10,8 +10,23 @@
             age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your age is: " + age);
             Console.ReadLine();
-            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age); // TryParse method
+            Console.WriteLine("Enter your age: ");
+            Int32.TryParse(Console.ReadLine(), out age); // TryParse method
             Console.WriteLine("Your age is: " + age);
+
+            // Part 2
+            double price, otherPrice;
+          
+
+            Console.WriteLine("Enter the price: ");
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The price is: " + price.ToString("C"));
+
+            Console.WriteLine("Enter the price: ");
+            double.TryParse(Console.ReadLine(), result: out price);
+            Console.WriteLine("The price is: " + price.ToString("C"));
+
+            
 
         }
     }
