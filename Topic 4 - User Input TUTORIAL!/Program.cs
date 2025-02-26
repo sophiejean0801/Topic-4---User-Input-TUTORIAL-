960daa135ff60f2239bbfac1dded665315268b46
Sophie Jean <363449299@GoTVDSB.ca>
2025-02-26 14:08:12 -0500

    Add project files.

---
+++ Topic 4 - User Input TUTORIAL!/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Topic_4___User_Input_TUTORIAL_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int age;
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadLine();
+            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age); // TryParse method
+            Console.WriteLine("Your age is: " + age);
+
+        }
+    }
+}
